3501fea2e3e6638338e74443c1a04f2033e17e66
Talal <tahmed530@gmail.com>
2020-12-01 14:07:43 +0500

    yearly report chart adjust

--- html/js/dashboard1.js
+++ html/js/dashboard1.js
@@ -257,7 +257,7 @@ $(function() {
         },
         bar: {
             width: {
-                ratio: 0.5 // this makes bar width 50% of length between ticks
+                ratio: 0.7 // this makes bar width 50% of length between ticks
             }
             // or
             //width: 100 // this makes bar width 100px
